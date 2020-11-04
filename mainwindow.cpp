f0cce59735105ebe6631d019c683a9202313979c
Craix <maciekszostak124@gmail.com>
2020-11-04 19:21:44 +0100

    Lab 2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,27 +11,76 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWin
     centralWidget()->layout()->setContentsMargins(0,0,0,0);
     centralWidget()->layout()->setSpacing(0);
 
-    ui->plot->plotColor=Qt::red;
-    ui->plot->plotData.resize(100);
+    connect(ui->actionRun, &QAction::triggered, this, &MainWindow::sendCommand);
+    connect(&serial, &QSerialPort::readyRead, this, &MainWindow::readData);
+    connect(&serial, SIGNAL(errorOccurred(QSerialPort::SerialPortError)), this, SLOT(error(QSerialPort::SerialPortError)));
+
+    ui->plot->plotData.resize(DSIZE2);
     ui->plot->plotData.fill(0);
 
-    for(int n=0; n<ui->plot->plotData.size(); n++)
-    {
-        ui->plot->plotData[n]=abs(sin(((3*M_PI*n))/static_cast<double>(ui->plot->plotData.size())));
-
-    }
-
-    ui->plot2->plotColor=Qt::green;
-    ui->plot2->plotData.resize(100);
+    ui->plot2->plotData.resize(DSIZE2);
     ui->plot2->plotData.fill(0);
 
-    for(int n=0; n<ui->plot2->plotData.size(); n++)
+    ui->statusbar->showMessage("No device");
+
+    QString portname;
+    const auto infos = QSerialPortInfo::availablePorts();
+    for (const QSerialPortInfo &info : infos)
     {
-        ui->plot2->plotData[n]= sin((3 * 2*M_PI*n)/static_cast<double>(ui->plot2->plotData.size()));
+        if (info.description()=="VCOM_MEAS")
+        {
+            portname=info.portName();
+            serial.setPortName(portname);
+            if (serial.open(QIODevice::ReadWrite))
+            {
+                ui->statusbar->showMessage(tr("Device: %1").arg(info.description()));
+                serial.clear();
+                ui->statusbar->setEnabled(true);
+            }
+            else
+            {
+                ui->statusbar->showMessage(tr("Can't open %1, error code %2") .arg(serial.portName()).arg(serial.error()));
+                qDebug() << "Hello world!";
+                return;
+            }
+
+            break;
+        }
     }
+}
+
+void MainWindow::sendCommand()
+{
+    senddata.clear();
+    senddata.resize(1);
+    senddata[0]=static_cast<uint8_t>(ui->actionRun->isChecked() << 7); // Run/Stop
+    serial.write(senddata);
+}
+
+void MainWindow::readData()
+{
+    if (serial.size() >= DSIZE) {
+
+        readdata=serial.read(DSIZE);
+        qDebug()<<readdata.size();
+
+        uint16_t *sample=reinterpret_cast<uint16_t*>(readdata.data());
+        for (int n=0; n<ui->plot->plotData.size(); n++) {
+            ui->plot->plotData[n]=(sample[n]-32768)/32768.0;
+            ui->plot2->plotData[n]= -1 * (sample[n] - 32768)/32768.0;
+        }
+
+        ui->plot->update();
+    }
+}
+
+void MainWindow::error(QSerialPort::SerialPortError error)
+{
+    qDebug()<<error;
 }
 
 MainWindow::~MainWindow()
 {
+    serial.close();
     delete ui;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+#include <QDebug>
+
+#define DSIZE 8192
+#define DSIZE2 (DSIZE/2)
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -10,12 +16,18 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
+private slots:
+    void sendCommand();
+    void readData();
+    void error(QSerialPort::SerialPortError error);
 private:
     Ui::MainWindow *ui;
+    QSerialPort serial;
+    QByteArray senddata;
+    QByteArray readdata;
 };
+
 #endif // MAINWINDOW_H
